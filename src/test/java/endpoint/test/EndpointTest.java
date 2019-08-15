b824413155e4e95b17fcf3e2eb8704311734f071
shilko2013 <shilko2013@mail.ru>
2019-08-15 15:31:04 +0300

    test methods signatures.

---
+++ src/test/java/endpoint/test/EndpointTest.java
@@ -0,0 +1,41 @@
+package endpoint.test;
+
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class EndpointTest {
+
+    @Test
+    public void testIllegalCompanyId() {
+
+    }
+
+    @Test
+    public void testIllegalUserName() {
+
+    }
+
+    @Test
+    public void testNonexistentCompanyId() {
+
+    }
+
+    @Test
+    public void testNonexistentUserName() {
+
+    }
+
+    @Test
+    public void testExistentUserName() {
+
+    }
+
+    @Test
+    public void testUserNamePermission() { //tests that username can be available only by his own company id
+
+    }
+
+}
